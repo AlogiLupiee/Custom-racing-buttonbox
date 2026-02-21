ece9082d3f15b1d070b1d1642492c6724acceb6c
AlogiLupiee <alogilupie@gmail.com>
2026-02-21 23:19:50 +0100

    Add CircuitPython gamepad firmware and HID driver

---
+++ code.py
@@ -0,0 +1,89 @@
+import board
+import digitalio
+import rotaryio
+import time
+import usb_hid
+import keypad
+from hid_gamepad import Gamepad
+
+
+
+# HUGE credits to MoemenMostafa on github. Most of this code is a modified version of his code. You can find it here: 
+# https://github.com/MoemenMostafa/button-box-circuitpython
+
+
+
+# HID setup
+gp = Gamepad(usb_hid.devices)
+
+# KeyMatrix setup
+km = keypad.KeyMatrix(
+    row_pins=(board.GP0, board.GP1),
+    column_pins=(board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7),
+)
+
+# Encoders setup
+encoder_pins = [(board.GP16, board.GP17), (board.GP19, board.GP20)]
+encoders = [rotaryio.IncrementalEncoder(pinA, pinB) for pinB, pinA in encoder_pins]
+
+encoder_buttons_pins = [board.GP18, board.GP21]
+encoder_buttons = [digitalio.DigitalInOut(pin) for pin in encoder_buttons_pins]
+for encoder_button in encoder_buttons:
+    encoder_button.direction = digitalio.Direction.INPUT
+    encoder_button.pull = digitalio.Pull.UP
+
+# keycodes for the buttons
+button_keycodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+# keycodes for the encoders
+encoder_keycodes = [[15, 16, 17], [18, 19, 20]]
+
+# Initialize encoder buttons state list with False value
+encoder_button_state = [False for i in range(len(encoder_buttons))]
+
+# Keypress and release
+def keyPress(key):
+    gp.press_buttons(key)
+
+def keyRelease(key):
+    gp.release_buttons(key)
+
+# Main loop
+while True:
+    # KeyMatrix reading and handling
+    event = km.events.get()
+    if event:
+        if event.pressed:
+            print(event)
+            keyPress(button_keycodes[event.key_number])
+        if event.released:
+            print(event)
+            keyRelease(button_keycodes[event.key_number])
+
+    # Encoders reading and handling
+    for idx, encoder in enumerate(encoders):
+        if encoder.position != 0:
+            if encoder.position > 0:
+                encoder.position = 1
+            if encoder.position < 0:
+                encoder.position = -1
+            position = encoder.position # copy the encoder position to set it to zero in next line
+            encoder.position = 0 # setting the encoder position to 0 before the sleep to protect againest rage clicks
+            print("Encoder {}: Position = {}".format(idx, position))
+            print("encoder_keycode = {}".format(encoder_keycodes[idx][position + 1]))
+            keyPress(encoder_keycodes[idx][position + 1])
+            time.sleep(0.1) # adding a 100ms sleep before key release to allow time to discover
+            keyRelease(encoder_keycodes[idx][position + 1])
+
+    # Encoder Buttons reading and handling
+    for idx, encoder_button in enumerate(encoder_buttons):
+        if not encoder_button.value and not encoder_button_state[idx]:
+            print("encoder_buttons {} Pressed".format(idx))
+            keyPress(encoder_keycodes[idx][1])
+            encoder_button_state[idx] = True
+        if encoder_button.value and encoder_button_state[idx]:
+            print("encoder_buttons {} Released".format(idx))
+            keyRelease(encoder_keycodes[idx][1])
+            encoder_button_state[idx] = False
+
+    time.sleep(0.01)  # Add a small delay to avoid flooding the USB connection
